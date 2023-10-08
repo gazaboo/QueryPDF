7bb111a9f94e1f02665b7acc57f2d64394899970
gazaboo <gazaboo@pop-os.localdomain>
2023-10-09 01:39:28 +0200

    feature : find reference

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 # from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import requests
 import os
 import torch
 import pandas as pd
 from sentence_transformers.util import semantic_search
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def main():
@@ -18,9 +18,11 @@ def main():
 
     if user_question:
         if 'dataset_embeddings' in st.session_state:
-            answers = handle_user_input(user_question)
-            for answer in answers:
+            answers_sourced = handle_user_input(user_question)
+            for answer, source in answers_sourced.items():
                 st.write(answer)
+                st.markdown(f':orange[Source : {source}]')
+                st.divider()
         else:
             st.write('Provide PDFs and click on process first')
 
@@ -30,10 +32,13 @@ def main():
             'Import here', accept_multiple_files=True)
         if st.button('Process'):
             with st.spinner('Processing'):
-                raw_text = get_pdf_text(pdf_docs)
+                raw_text, meta_infos = get_pdf_text(pdf_docs)
+                st.session_state.meta_infos = meta_infos
+
                 text_chunks = get_text_chunks(raw_text)
                 embeddings = query(text_chunks)
                 embeddings = pd.DataFrame(embeddings)
+
                 st.session_state.text_chunks = text_chunks
                 st.session_state.dataset_embeddings = torch.from_numpy(
                     embeddings.to_numpy()).to(torch.float)
@@ -45,11 +50,20 @@ def handle_user_input(input):
     query_embedding = query(user_query)
     query_embedding = torch.from_numpy(
         pd.DataFrame(query_embedding).to_numpy()).to(torch.float)
+
     hits = semantic_search(
         query_embedding, st.session_state.dataset_embeddings, top_k=5)
-    answer = [st.session_state.text_chunks[hits[0][i]['corpus_id']]
-              for i in range(len(hits[0]))]
-    return answer
+
+    ids = [hit['corpus_id'] for hit in hits[0]]
+    answers = [st.session_state.text_chunks[id_chunk] for id_chunk in ids]
+
+    sources = []
+    for chunk in answers:
+        for text, name in st.session_state.meta_infos.items():
+            if chunk in text:
+                sources.append(name)
+
+    return {answer: source for answer, source in zip(answers, sources)}
 
 
 def query(texts):
@@ -73,11 +87,14 @@ def get_text_chunks(text):
 
 def get_pdf_text(pdf_docs):
     text = ''
+    meta_info = dict()
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text().replace('\n', ' ')
-    return text
+            meta_info[page.extract_text().replace('\n', ' ')] = pdf.name
+
+    return text, meta_info
 
 
 if __name__ == '__main__':
